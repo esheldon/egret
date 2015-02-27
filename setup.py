a9cfc568423bccefe13e6b831473799b6eede043
Matthew R Becker <becker.mr@gmail.com>
2015-02-27 00:10:32 -0800

    added setup to make it a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+import glob
+from distutils.core import setup
+
+setup(name="justok_des_makers",
+            version="0.1",
+            description="code to make DES-like GREAT3-like sims",
+            license = "3-clause BSD",
+            author="Matthew R. Becker w/ code from GREAT3 team",
+            author_email="becker.mr@gmail.com",
+            packages=['justok_des_makers','justok_des_makers.great3_cosmos_gals'])
