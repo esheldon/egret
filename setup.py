c3982cee9b594d45d598361a7f6322d4eeaac154
Matthew R Becker <becker.mr@gmail.com>
2015-02-27 00:14:32 -0800

    redid module setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import os
 import glob
 from distutils.core import setup
 
-setup(name="justok_des_makers",
+setup(name="justok_des",
             version="0.1",
             description="code to make DES-like GREAT3-like sims",
             license = "3-clause BSD",
             author="Matthew R. Becker w/ code from GREAT3 team",
             author_email="becker.mr@gmail.com",
-            packages=['justok_des_makers','justok_des_makers.great3_cosmos_gals'])
+            packages=['justok_des','justok_des.makers','justok_des.makers.great3_cosmos_gals'])
