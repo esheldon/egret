3a8210340d94ca6c4476a97cf7c4bc0effcd459a
Matthew R. Becker <beckermr@users.noreply.github.com>
2015-08-20 10:26:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import os
 import glob
 from distutils.core import setup
 
-setup(name="lensing_sims_tools",
+setup(name="egret",
             version="0.1",
             description="code to make lensing sims",
             license = "3-clause BSD",
             author="Matthew R. Becker w/ code from GREAT3 team",
             author_email="becker.mr@gmail.com",
-            packages=['lensing_sims_tools','lensing_sims_tools.makers','lensing_sims_tools.blendmakers','lensing_sims_tools.makers.great3_cosmos_gals'])
+            packages=['egret','egret.makers',
+                      'egret.blendmakers',
+                      'egret.makers.great3_cosmos_gals'])
